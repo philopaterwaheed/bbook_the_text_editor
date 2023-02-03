46a939086a08dd63da8a56c515afd1dbc663f1a4
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-02-03 01:08:09 +0000

    Add files via upload

--- buffer.cpp
+++ buffer.cpp
@@ -41,16 +41,25 @@ void buf::removeLine(int n)
     lines.erase(lines.begin()+n); // removes the nth line 
     lines.shrink_to_fit();
 }
-std::string buf::  copy_line (int c_char ){
-    temp_line += lines[line][c_char] ;
+void  buf::  copy_line (int sch , int fch ,int sl ,int fli  ){
+    this->lines_paste = getStrings_from_lines(false , sch , fch , sl , fli) ; 
 }
 
-std::string buf::  cut_line (std::string){
-    temp_line = lines[line] ;
-    removeLine(line); 
+std::string buf::  cut_line  (int sch , int fch ,int sl ,int fli ){
+    this->lines_paste = getStrings_from_lines(true , sch , fch , sl , fli) ; 
 }
-std::string buf::  paste_line (int line , int x ){
-   injectIntoLine("temp_line",x);
+std::string buf::  paste_line  (int line , std :: vector  <std::string> &lines_paste,int x ){
+    int size = lines_paste.size() ; 
+    if (size== 1 )
+        injectIntoLine(lines_paste[0],x);
+    else 
+        {
+            lines_paste.push_back(lines[line].substr(x,lines[line].size()));
+            lines[line].erase(x,lines[line].size());
+            size = lines_paste.size() ; 
+            for (int i = 0 ; i<size; i++ )
+                lines.insert(lines.begin()+line+i+1,lines_paste[i]);
+        }
 }
 
 int buf:: space_for_numbers( int size ){
@@ -61,4 +70,58 @@ int buf:: space_for_numbers( int size ){
         count++ ;
     }
     return count+2;
+}
+std :: vector <std::string> buf :: getStrings_from_lines (bool cut  , int startc , int endc , int startl , int endl ) 
+{   
+    std :: vector <std::string> output = {}; 
+    if (!cut )
+        {
+            if (startl== endl)
+                output.push_back( lines[startl].substr(startc , endc+1) ) ;
+            else 
+                for (int i = startl ; i <= endl ;  i++)
+                    {
+                        if ( i == startl)
+                            output.push_back(lines[startl].substr(startc)) ;
+                        else if ( i == endl )
+                            output.push_back( lines[endl].substr(0, endc+1) ) ;
+                        else 
+                            output.push_back(lines[i]);
+                    }
+        }
+    else 
+        {
+            if (startl== endl)
+                {
+                    output.push_back( lines[startl].substr(startc , endc+1) ) ;
+                    lines[startl].erase(startc , endc+1) ; 
+                }
+            else 
+                for (int i = startl ; i <= endl ;  i++)
+                    {
+                        if ( i == startl)
+                        {
+                            output.push_back(lines[startl].substr(startc)) ;
+                            if (startc == 0 )
+                            lines.erase(lines.begin()+startl);
+                            else 
+                            lines[startl].erase(startc) ;
+                        }
+                else if ( i == endl )   
+                        {
+                            output.push_back( lines[endl].substr(0 , endc+1) ) ;
+                            if (endc == lines[endl].size() )
+                            lines.erase(lines.begin()+endl);
+                            else 
+                            lines[endl].erase(0 , endc+1) ;
+                        }
+                        else
+                            {
+                                output.push_back(lines[line]);
+                                lines.erase(lines.begin()+startl);
+                            }
+                    }
+        }
+    return  output ; 
+
 }
